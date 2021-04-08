1827404bc8284f0ace993ea7b5348996a02f33f0
Guiyun <my@guiyunweb.com>
2021-04-09 00:15:30 +0800

    fix:添加查询

--- app/system/index/internal/api/index.go
+++ app/system/index/internal/api/index.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"blog/app/dao"
 	"blog/app/model"
 	"blog/app/system/index/internal/define"
 	"blog/app/system/index/internal/service"
@@ -11,11 +12,12 @@ func Index(r *ghttp.Request) {
 	var (
 		data *define.ContentDetailReq
 	)
+	list, _ := dao.Posts.FindAll()
 	if err := r.Parse(&data); err != nil {
 		service.View.Render(r, model.View{
 			Title:   "首页",
 			MainTpl: "index.tpl",
-			Data:    data,
+			Data:    list,
 		})
 	}
 }
