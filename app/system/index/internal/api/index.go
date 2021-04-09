ab65379b926d7dcc3fc63050257aeb9002194edd
Guiyun <my@guiyunweb.com>
2021-04-09 13:30:55 +0800

    feat:添加分页

--- app/system/index/internal/api/index.go
+++ app/system/index/internal/api/index.go
@@ -5,19 +5,24 @@ import (
 	"blog/app/model"
 	"blog/app/system/index/internal/define"
 	"blog/app/system/index/internal/service"
+	"github.com/gogf/gf/frame/g"
 	"github.com/gogf/gf/net/ghttp"
 )
 
 func Index(r *ghttp.Request) {
 	var (
-		data *define.ContentDetailReq
+		data *define.ContentPageReq
 	)
-	list, _ := dao.Posts.FindAll()
 	if err := r.Parse(&data); err != nil {
+		list, _ := dao.Posts.Limit(data.Page, 10).FindAll(g.Map{
+			"type": 0,
+		})
 		service.View.Render(r, model.View{
 			Title:   "首页",
 			MainTpl: "index.tpl",
 			Data:    list,
 		})
+	} else {
+		r.Response.Writeln("499")
 	}
 }
--- app/system/index/internal/define/content.go
+++ app/system/index/internal/define/content.go
@@ -4,3 +4,7 @@ type ContentDetailReq struct {
 	Id   uint `v:"min:1#请选择查看的内容"`
 	Page uint `v:"min:1#请选择查看的内容"`
 }
+
+type ContentPageReq struct {
+	Page int `v:"min:1#请选择选择分页"`
+}
